c283ce18781e5adc92fc53657a00897273c043f3
Alan <ahernandezpe@utem.cl>
2025-10-21 18:17:19 -0300

    pagina principal

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from .views import inicio
 from . import views
 
 urlpatterns = [
-    path('', inicio, name='inicio'),
-     path('catalogo/', views.catalogo, name='catalogo'),
+    path('', views.inicio, name='inicio'),
+    path('catalogo/', views.catalogo, name='catalogo'),
 ]
