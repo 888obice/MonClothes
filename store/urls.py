877bd2229665dbd5074976010f88fa818f28c65c
Alan <ahernandezpe@utem.cl>
2025-10-22 20:17:20 -0300

    mejoras

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio,tienda
 from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('catalogo/', views.catalogo, name='catalogo'),
+    path('',views.tienda, name='tienda')
 ]
